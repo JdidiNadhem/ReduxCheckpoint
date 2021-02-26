308d6d05868713bfc79c2ae8fa73e6fe5cff3060
Jdidi Nadhem <jedidinadhem@gmail.com>
2021-02-26 15:30:46 +0100

    first adding

---
+++ src/Component/Task/Task.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import "./Task.css";
+import {useDispatch} from "react-redux";
+import {deleteTask, doneTask, editTask} from "../../JS/Actions/Task";
+
+const Task = ({description, id, isDone}) => {
+  const dispatch = useDispatch();
+  // Hook to verif if edit button is clicked or not
+  const [open_edit, setopen_verif_edit] = useState(false);
+  // Hook to store the input value
+  const [edit_desc, setedit_desc] = useState("");
+  return (
+    <div className="Task">
+      {!open_edit ? (
+        <textarea
+          className={isDone ? "done desc" : "undone desc"}
+          rows="10"
+          cols="33"
+          value={description}
+          readOnly={true}
+        ></textarea>
+      ) : (
+        // if user choose to edit task
+        <input
+          className="desc"
+          defaultValue={description}
+          type="text"
+          onChange={(e) => {
+            setedit_desc(e.target.value);
+          }}
+        />
+      )}
+      <button
+        onClick={() => {
+          if (!open_edit) {
+            setopen_verif_edit(!open_edit);
+            setedit_desc(description);
+          } else {
+            dispatch(editTask({id, description: edit_desc}));
+            setedit_desc("");
+            setopen_verif_edit(!open_edit);
+          }
+        }}
+      >
+        Edit
+      </button>
+      <button onClick={() => dispatch(deleteTask(id))}>DELETE</button>
+      <button onClick={() => dispatch(doneTask(id))}>
+        {isDone ? "Done" : "Undone"}
+      </button>
+    </div>
+  );
+};
+
+export default Task;
